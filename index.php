34f954f41c8a2d5c6610db188f6979187124b891
edinugraha <edinugraha20@gmail.com>
2013-08-07 12:10:13 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+</tr>
+<tr>
+<td height="24">
+<form name="chat" id="chating">
+nama : <input type="text" name="nama" size="15" /><br />
+<input id="isiChat" type="text" name="isiChat" size="30" onChange="kirim()" />
+<input type="button" value="kirim" onClick="kirim()" />
+</form>
+</td>
+</tr>
+</table>
+</body>
+</html>
